1bfa7c6ec23d932771ec982d6275bdaa3b6b5c26
Robin Phillips <robinphillips462@gmail.com>
2026-08-23 11:31:16 -0400

    Implement FlashcardAppDatabase

--- app/src/main/java/io/github/robinphillips98/flashcards/data/AppContainer.kt
+++ app/src/main/java/io/github/robinphillips98/flashcards/data/AppContainer.kt
@@ -1,20 +1,30 @@
 package io.github.robinphillips98.flashcards.data
 
 import android.content.Context
+import io.github.robinphillips98.flashcards.data.decks.DecksRepository
+import io.github.robinphillips98.flashcards.data.decks.RoomDeckRepository
+import io.github.robinphillips98.flashcards.data.flashcards.FlashcardsRepository
+import io.github.robinphillips98.flashcards.data.flashcards.RoomFlashcardsRepository
 
 interface AppContainer {
-    val deckDatasource: DeckDatasource // TODO: Change to DeckRepository when I implement a database
-    val flashcardDatasource: FlashcardDatasource // TODO: Change to FlashcardRepository when I implement a database
+    val decksRepository: DecksRepository
+    val flashcardsRepository: FlashcardsRepository
 }
 
 class AppDataContainer(private val context: Context): AppContainer {
-    override val deckDatasource: DeckDatasource by lazy {
-        DeckDatasource()
+    override val decksRepository: DecksRepository by lazy {
+        RoomDeckRepository(
+            FlashcardAppDatabase
+                .getDatabaseInstance(context)
+                .deckDao()
+        )
     }
 
-    override val flashcardDatasource: FlashcardDatasource by lazy {
-        FlashcardDatasource()
+    override val flashcardsRepository: FlashcardsRepository by lazy {
+        RoomFlashcardsRepository(
+            FlashcardAppDatabase
+                .getDatabaseInstance(context)
+                .flashcardDao()
+        )
     }
-
-
 }
